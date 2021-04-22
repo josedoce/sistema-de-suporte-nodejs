3c67b5fb30df7bbdd2eb149a8c07543ac47eb2eb
josedoce <josesamya@gmail.com>
2021-04-22 10:08:58 -0300

    dia3: Adição de novas rotas.

--- src/routes.ts
+++ src/routes.ts
@@ -1,7 +1,12 @@
 import {Router} from 'express';
+import MessagesController from './controllers/MessagesController';
 import SettingsController from './controllers/SettingsController';
+import UserController from './controllers/UserController';
 const routes = Router();
 
 routes.post('/settings', SettingsController.index)
+routes.post('/users', UserController.index)
+routes.post('/messages', MessagesController.index)
+routes.get('/messages/:id', MessagesController.showByUser)
 
 export {routes};
