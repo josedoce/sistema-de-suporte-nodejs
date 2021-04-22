30acfe49edc9a94193391afd12c61df5f43f46b3
josedoce <josesamya@gmail.com>
2021-04-22 10:07:50 -0300

    dia3: criação de messages e user repository

--- src/repositories/SettingsRepository.ts
+++ src/repositories/SettingsRepository.ts
@@ -3,11 +3,6 @@ import { Setting } from "../entities/Setting";
 
 @EntityRepository(Setting)
 class SettingsRepository extends Repository<Setting>{
-    async makeSave(data: object):Promise<Setting>{
-        const settings = this.create(data)
-        return await this.save(settings);   
-    }
 }
 
-interface SettingsRepositoryInterface extends SettingsRepository{}
-export { SettingsRepository, SettingsRepositoryInterface };
+export { SettingsRepository };
