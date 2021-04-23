45a8864888c1f8fae174de15ae089cf192250c88
josedoce <josesamya@gmail.com>
2021-04-23 03:58:25 -0300

    dia4: novas rotas foram adicionadas

--- src/routes.ts
+++ src/routes.ts
@@ -4,9 +4,11 @@ import SettingsController from './controllers/SettingsController';
 import UserController from './controllers/UserController';
 const routes = Router();
 
-routes.post('/settings', SettingsController.index)
-routes.post('/users', UserController.index)
-routes.post('/messages', MessagesController.index)
-routes.get('/messages/:id', MessagesController.showByUser)
+routes.post('/settings', SettingsController.index);
+routes.get('/settings/:username', SettingsController.findByUserName);
+routes.put('/settings/:username', SettingsController.update);
+routes.post('/users', UserController.index);
+routes.post('/messages', MessagesController.index);
+routes.get('/messages/:id', MessagesController.showByUser);
 
 export {routes};
